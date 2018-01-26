dc188f0ee9805b642ade39228ac7198d25cde576
OneYX <504887284@qq.com>
2018-01-26 21:09:36 +0800

    no message

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,40 +1,33 @@
 #!/bin/sh
 
+#
+# print a message to console with 
+#
+printMsg()
+{
+  echo $(for i in $(seq 1 80);do echo -n "-";done;);  
+  echo $1;
+  echo $(for i in $(seq 1 80);do echo -n "-";done;);
+}
+
 # replace sources with aliyun mirrors
 sed -i -e 's/\(us\.\)\?archive.ubuntu.com\|security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
-
-# for install sogoupinyin
-mkdir -p /etc/apt/sources.list.d
-touch /etc/apt/sources.list.d/sogoupinyin.list
-sh -c "echo 'deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' >> /etc/apt/sources.list.d/sogoupinyin.list"
-
-# change zoneinfo to Shanghai
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 # add ppa to system's Software Sources
 add-apt-repository -y ppa:webupd8team/java
 add-apt-repository -y ppa:gnome3-team/gnome3
 add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 
-# update the system
+printMsg "update ......"
 apt-get update
-apt-get upgrade
 
 ################################################################################
 # Install the mandatory tools
 ################################################################################
 
-export LANGUAGE='zh_CN.UTF-8'
-export LANG='zh_CN.UTF-8'
-export LC_ALL='zh_CN.UTF-8'
-locale-gen zh_CN.UTF-8
-dpkg-reconfigure locales
-
 # install utilities
+printMsg "install utilities"
 apt-get -y install vim git zip bzip2 fontconfig curl language-pack-en language-pack-zh-hans language-pack-zh-hans-base language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base
-
-# install sogoupinyin
-apt-get -y --allow-unauthenticated install sogoupinyin
 
 # install Java 8
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
@@ -54,7 +47,15 @@ echo 'LC_CTYPE=zh_CN.UTF-8' >> /etc/environment
 echo 'allowed_users=anybody' > /etc/X11/Xwrapper.config
 
 # install Ubuntu desktop and VirtualBox guest tools
-apt-get install -y gnome-shell ubuntu-gnome-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+printMsg "install Ubuntu desktop and VirtualBox guest tools"
+apt-get update && apt-get install -y gnome-shell ubuntu-gnome-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
+# for install sogoupinyin
+sh -c "echo 'deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main' > /etc/apt/sources.list.d/sogoupinyin.list"
+
+# install sogoupinyin
+printMsg "install sogoupinyin"
+apt-get update && apt-get -y --allow-unauthenticated install sogoupinyin
 
 ################################################################################
 # Install the development tools
@@ -104,9 +105,43 @@ sysctl -p --system
 # fix ownership of home
 chown -R vagrant:vagrant /home/vagrant/
 
+printMsg "clean the box"
 # clean the box
 apt-get -y autoclean
 apt-get -y clean
 apt-get -y autoremove
+
+printMsg "remove apt files, docs, old kernels, history, logs"
+# Remove APT files
+find /var/lib/apt/lists -type f | xargs rm -f
+# Clear cache
+find /var/cache -type f -exec rm -rf {} \;
+# Clear docs
+shopt -s extglob
+rm -rf /usr/share/doc-base/*
+# remove old kernels
+dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
+# Remove bash history
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+# Cleanup log files
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+printMsg "Write zero in free space"
+# Zero free space to aid VM compression
 dd if=/dev/zero of=/EMPTY bs=1M > /dev/null 2>&1
 rm -f /EMPTY
+
+#
+# update time zone
+#
+
+# based on http://askubuntu.com/questions/323131/setting-timezone-from-terminal
+printMsg "Setting TimeZone..."
+export TZ=`wget -qO - http://geoip.ubuntu.com/lookup | sed -n -e 's/.*<TimeZone>\(.*\)<\/TimeZone>.*/\1/p'` && timedatectl set-timezone $TZ
+export TZ=`timedatectl status| grep 'Time zone' | awk '{print $3}'`
+printMsg "TimeZone set to $TZ"
+
+# change language to Chinese(simplified)
+localectl set-locale LANG=zh_CN.UTF-8
